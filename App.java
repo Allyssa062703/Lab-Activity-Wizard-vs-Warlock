a86d39447419ada38eb60ec1150651c57ed38f38
allyssamyr <Allyssamyrrrr0627@gmail.com>
2022-05-13 21:39:46 +0800

    Create App.java

---
+++ App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Battle Begin!");
+        System.out.println("Wizard and Warlock");
+        System.out.println("Both Characters Have 500 HP and 500 Mana");
+        System.out.println("~~~~~~~~~~");
+
+        Wizards Rick = new Wizards("Rick");
+        Warlocks Morty = new Warlock("Morty");
+        Rick.Uppercut(Sasuke);
+        Rick.punch(Sasuke);
+        Morty.onepunch(Rick);
+        Rick.slap(Morty);
+        Morty.powerpunch(Rick);
+        Morty.hamehamewave(Rick);
+        Morty.slappunch (Rick);
+        Rick.jab(Rick);
+        Morty.headbutt (Rick);
+        Rick.powerslap(Morty);
+
+    }
+}
